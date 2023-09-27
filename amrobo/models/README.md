93ea2cffec16662d5723f7485b9c59bf294b22f9
Krishna Suresh <krishna4132@gmail.com>
2023-09-26 22:05:12 -0400

    started scope station

---
+++ amrobo/models/README.md
@@ -0,0 +1,12 @@
+
+
+# TODO Environments
+5 camera setup 
+3 camera setup scope space with extrusion 
+camera+table
+camera+bin
+gen3 + 2f_85
+gen3 + powerpick
+
+arm+grip+camera+table
+arm+grip+camera+bin
--- amrobo/scope_station.py
+++ amrobo/scope_station.py
@@ -26,7 +26,6 @@ from pydrake.all import (
     GeometryInstance,
     InverseDynamicsController,
     LeafSystem,
-    MakeMultibodyStateToWsgStateSystem,
     MakePhongIllustrationProperties,
     MakeRenderEngineVtk,
     ModelInstanceIndex,
@@ -42,21 +41,17 @@ from pydrake.all import (
     RigidTransform,
     RollPitchYaw,
     RotationMatrix,
-    SchunkWsgPositionController,
     SpatialInertia,
     Sphere,
     StateInterpolatorWithDiscreteDerivative,
     UnitInertia,
 )
 
+from .utils import ConfigureParser
+
 def MakeScopeStation(
     model_directives,
     time_step=0.002,
-    iiwa_prefix="iiwa",
-    wsg_prefix="wsg",
-    camera_prefix="camera",
-    prefinalize_callback=None,
-    package_xmls=[],
 ):
     builder = DiagramBuilder()
 
@@ -65,178 +60,25 @@ def MakeScopeStation(
         builder, time_step=time_step
     )
     parser = Parser(plant)
-    for p in package_xmls:
-        parser.package_map().AddPackageXml(p)
     ConfigureParser(parser)
     parser.AddModelsFromString(model_directives, ".dmd.yaml")
-    if prefinalize_callback:
-        prefinalize_callback(plant)
     plant.Finalize()
 
     for i in range(plant.num_model_instances()):
         model_instance = ModelInstanceIndex(i)
         model_instance_name = plant.GetModelInstanceName(model_instance)
 
-        if model_instance_name.startswith(iiwa_prefix):
-            num_iiwa_positions = plant.num_positions(model_instance)
-
-            # I need a PassThrough system so that I can export the input port.
-            iiwa_position = builder.AddSystem(PassThrough(num_iiwa_positions))
-            builder.ExportInput(
-                iiwa_position.get_input_port(),
-                model_instance_name + "_position",
-            )
-            builder.ExportOutput(
-                iiwa_position.get_output_port(),
-                model_instance_name + "_position_commanded",
-            )
-
-            # Export the iiwa "state" outputs.
-            demux = builder.AddSystem(
-                Demultiplexer(2 * num_iiwa_positions, num_iiwa_positions)
-            )
-            builder.Connect(
-                plant.get_state_output_port(model_instance),
-                demux.get_input_port(),
-            )
-            builder.ExportOutput(
-                demux.get_output_port(0),
-                model_instance_name + "_position_measured",
-            )
-            builder.ExportOutput(
-                demux.get_output_port(1),
-                model_instance_name + "_velocity_estimated",
-            )
-            builder.ExportOutput(
-                plant.get_state_output_port(model_instance),
-                model_instance_name + "_state_estimated",
-            )
-
-            # Make the plant for the iiwa controller to use.
-            controller_plant = MultibodyPlant(time_step=time_step)
-            # TODO: Add the correct IIWA model (introspected from MBP)
-            if plant.num_positions(model_instance) == 3:
-                controller_iiwa = AddPlanarIiwa(controller_plant)
-            else:
-                controller_iiwa = AddIiwa(controller_plant)
-            AddWsg(controller_plant, controller_iiwa, welded=True)
-            controller_plant.Finalize()
-
-            # Add the iiwa controller
-            iiwa_controller = builder.AddSystem(
-                InverseDynamicsController(
-                    controller_plant,
-                    kp=[100] * num_iiwa_positions,
-                    ki=[1] * num_iiwa_positions,
-                    kd=[20] * num_iiwa_positions,
-                    has_reference_acceleration=False,
-                )
-            )
-            iiwa_controller.set_name(model_instance_name + "_controller")
-            builder.Connect(
-                plant.get_state_output_port(model_instance),
-                iiwa_controller.get_input_port_estimated_state(),
-            )
-
-            # Add in the feed-forward torque
-            adder = builder.AddSystem(Adder(2, num_iiwa_positions))
-            builder.Connect(
-                iiwa_controller.get_output_port_control(),
-                adder.get_input_port(0),
-            )
-            # Use a PassThrough to make the port optional (it will provide zero
-            # values if not connected).
-            torque_passthrough = builder.AddSystem(
-                PassThrough([0] * num_iiwa_positions)
-            )
-            builder.Connect(
-                torque_passthrough.get_output_port(), adder.get_input_port(1)
-            )
-            builder.ExportInput(
-                torque_passthrough.get_input_port(),
-                model_instance_name + "_feedforward_torque",
-            )
-            builder.Connect(
-                adder.get_output_port(),
-                plant.get_actuation_input_port(model_instance),
-            )
-
-            # Add discrete derivative to command velocities.
-            desired_state_from_position = builder.AddSystem(
-                StateInterpolatorWithDiscreteDerivative(
-                    num_iiwa_positions,
-                    time_step,
-                    suppress_initial_transient=True,
-                )
-            )
-            desired_state_from_position.set_name(
-                model_instance_name + "_desired_state_from_position"
-            )
-            builder.Connect(
-                desired_state_from_position.get_output_port(),
-                iiwa_controller.get_input_port_desired_state(),
-            )
-            builder.Connect(
-                iiwa_position.get_output_port(),
-                desired_state_from_position.get_input_port(),
-            )
-
-            # Export commanded torques.
-            builder.ExportOutput(
-                adder.get_output_port(),
-                model_instance_name + "_torque_commanded",
-            )
-            builder.ExportOutput(
-                adder.get_output_port(),
-                model_instance_name + "_torque_measured",
-            )
-
-            builder.ExportOutput(
-                plant.get_generalized_contact_forces_output_port(
-                    model_instance
-                ),
-                model_instance_name + "_torque_external",
-            )
-
-        elif model_instance_name.startswith(wsg_prefix):
-            # Wsg controller.
-            wsg_controller = builder.AddSystem(SchunkWsgPositionController())
-            wsg_controller.set_name(model_instance_name + "_controller")
-            builder.Connect(
-                wsg_controller.get_generalized_force_output_port(),
-                plant.get_actuation_input_port(model_instance),
-            )
-            builder.Connect(
-                plant.get_state_output_port(model_instance),
-                wsg_controller.get_state_input_port(),
-            )
-            builder.ExportInput(
-                wsg_controller.get_desired_position_input_port(),
-                model_instance_name + "_position",
-            )
-            builder.ExportInput(
-                wsg_controller.get_force_limit_input_port(),
-                model_instance_name + "_force_limit",
-            )
-            wsg_mbp_state_to_wsg_state = builder.AddSystem(
-                MakeMultibodyStateToWsgStateSystem()
-            )
-            builder.Connect(
-                plant.get_state_output_port(model_instance),
-                wsg_mbp_state_to_wsg_state.get_input_port(),
-            )
-            builder.ExportOutput(
-                wsg_mbp_state_to_wsg_state.get_output_port(),
-                model_instance_name + "_state_measured",
-            )
-            builder.ExportOutput(
-                wsg_controller.get_grip_force_output_port(),
-                model_instance_name + "_force_measured",
-            )
+        if model_instance_name.startswith("gen3"): # TODO real prefix 
+            # TODO
+            pass
+        elif model_instance_name.startswith("2f_85"): # TODO real prefix
+            pass
+        elif model_instance_name.startswith("powerpick"): # TODO real prefix
+            pass
 
     # Cameras.
     AddRgbdSensors(
-        builder, plant, scene_graph, model_instance_prefix=camera_prefix
+        builder, plant, scene_graph
     )
 
     # Export "cheat" ports.
