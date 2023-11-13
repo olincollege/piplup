d629d55681ea46ae2e50d45649c9706f64e6cabc
Krishna Suresh <krishna4132@gmail.com>
2023-11-13 09:08:47 -0500

    added stubgen script

---
+++ realsense/stubgen.py
@@ -0,0 +1,12 @@
+import importlib
+import os
+from mypy import stubgen
+import sys
+
+if __name__ == "__main__":
+    module = sys.argv[1]
+    importlib.__import__(module)
+    args = [ "--output=.", f"--module={module}"]
+    returncode = stubgen.main(args=args) or 0
+    assert returncode == 0, returncode
+    print(os.getcwd())
