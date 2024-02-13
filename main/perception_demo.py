a11ab2684b2483733f59b5b1a2e5dd36af20f38e
Krishna Suresh <krishna4132@gmail.com>
2024-02-13 17:05:52 -0500

    grasp selector outputs ee pose

--- main/perception_demo.py
+++ main/perception_demo.py
@@ -117,7 +117,7 @@ class SuctionGraspSelector(LeafSystem):
         self.DeclareAbstractOutputPort(
             "grasp_selection",
             lambda: AbstractValue.Make((np.inf, RigidTransform())),
-            self.SelectGrasp,
+            self.GenerateGrasp,
         )
 
         self._rng = np.random.default_rng()
@@ -129,13 +129,26 @@ class SuctionGraspSelector(LeafSystem):
             rgba=Rgba(0, 1, 0, 0.25),
         )
 
-    def SelectGrasp(self, context: Context, output: AbstractValue):
+        self.X_end_to_tool_inv = RigidTransform(
+            AngleAxis(np.pi, np.array([1, 0, 0])), np.array([0, 0, 0.06])
+        )
+
+    def GenerateGrasp(self, context: Context, output: AbstractValue):
         pc: PointCloud = self.point_cloud_port_.Eval(context)
+
+        centroid = np.mean(pc.xyzs(), axis=1)
+        AddMeshcatTriad(
+            self.meshcat,
+            "/centroid",
+            length=0.05,
+            radius=0.003,
+            X_PT=RigidTransform(centroid),
+        )
 
         candidates = []
         scores = []
         crops = []
-        for i in range(100):
+        for i in range(300):
             index = self._rng.integers(0, pc.size() - 1)
             p_WS = pc.xyz(index)
             n_WS = pc.normal(index)
@@ -155,14 +168,17 @@ class SuctionGraspSelector(LeafSystem):
             )
             p1 = np.minimum(lower, upper)
             p2 = np.maximum(lower, upper)
-
+            print(p1)
+            print(p2)
+            if np.all(p1 > p2) or np.any(np.isnan(p1)) or np.any(np.isnan(p2)):
+                continue
             cropped = pc.Crop(p_WS + p1, p_WS + p2)
 
             if (
                 cropped.size() > 70
                 and np.abs(np.dot(np.array([0.0, 0.0, 1.0]), n_WS)) > 0.8
             ):
-                score = p_WS[2]
+                score = p_WS[2] + 1 / np.linalg.norm(p_WS[:2] - centroid[:2])
                 scores.append(score)
                 candidates.append(RigidTransform(R_WG, p_WS))
                 crops.append(cropped)
@@ -170,13 +186,24 @@ class SuctionGraspSelector(LeafSystem):
         if scores:
             i = np.array(scores).argmax()
             G = candidates[i]
-            AddMeshcatTriad(self.meshcat, "/test", length=0.02, radius=0.001, X_PT=G)
             cropped = crops[i]
+
             self.meshcat.SetObject(
                 "/cropped", cropped, point_size=0.003, rgba=Rgba(0, 1, 0, 1)
             )
             self.meshcat.SetTransform("/cropping_box", G)
-            output.set_value((scores[i], G))
+            AddMeshcatTriad(
+                self.meshcat, "/pick_point", length=0.02, radius=0.001, X_PT=G
+            )
+            AddMeshcatTriad(
+                self.meshcat,
+                "/ee_pose",
+                length=0.02,
+                radius=0.001,
+                X_PT=G @ self.X_end_to_tool_inv,
+            )
+            # X_WT = X_WE * X_ET
+            output.set_value((scores[i], G @ self.X_end_to_tool_inv))
         else:
             output.set_value((0, RigidTransform()))
 
