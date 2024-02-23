a092b3891d981198b237c7e57b8705fe0461ff42
Krishna Suresh <krishna4132@gmail.com>
2024-02-23 00:54:49 -0500

    started gen3 c++

--- kinova_gen3/gen3_interface.cc
+++ kinova_gen3/gen3_interface.cc
@@ -1,15 +1,49 @@
-#define _OS_UNIX
+#include "kinova_gen3/gen3_interface.h"
 
-#include <BaseClientRpc.h>
-#include <Common.pb.h>
-#include <DeviceConfigClientRpc.h>
-#include <DeviceManagerClientRpc.h>
-#include <RouterClient.h>
-#include <SessionManager.h>
-#include <TransportClientTcp.h>
-
-int main(int argc, char const * argv[])
+namespace piplup
 {
-    /* code */
-    return 0;
-}
+    namespace kinova_gen3
+    {
+        using namespace drake;
+        namespace k_api = Kinova::Api;
+        Gen3HardwareInterface::Gen3HardwareInterface()
+        {
+            double hz = 40.0;
+            DeclareInitializationUnrestrictedUpdateEvent(
+                &Gen3HardwareInterface::Initialize);
+            DeclarePeriodicUnrestrictedUpdateEvent(
+                1 / hz, 0.0, &Gen3HardwareInterface::CalcUpdate);
+        }
+        systems::EventStatus Gen3HardwareInterface::Initialize(
+            const systems::Context<double> &, systems::State<double> *)
+        {
+            auto error_callback = [](k_api::KError err) {
+                cout << "_________ callback error _________" << err.toString();
+            };
+            transport = new k_api::TransportClientTcp();
+            router = new k_api::RouterClient(transport, error_callback);
+            transport->connect("192.168.1.10", 10000);
+
+            // Set session data connection information
+            auto create_session_info = k_api::Session::CreateSessionInfo();
+            create_session_info.set_username("admin");
+            create_session_info.set_password("admin");
+            create_session_info.set_session_inactivity_timeout(60000);   // (milliseconds)
+            create_session_info.set_connection_inactivity_timeout(2000); // (milliseconds)
+
+            // Session manager service wrapper
+            std::cout << "Creating session for communication" << std::endl;
+            session_manager = new k_api::SessionManager(router);
+            session_manager->CreateSession(create_session_info);
+            std::cout << "Session created" << std::endl;
+
+            // Create services
+            base_ = new k_api::Base::BaseClient(router);
+            return systems::EventStatus::Succeeded();
+        }
+
+        void Gen3HardwareInterface::CalcUpdate(const systems::Context<double> &,
+                                               systems::State<double> *) const
+        {}
+    } // namespace kinova_gen3
+} // namespace piplup
---
+++ kinova_gen3/gen3_interface.h
@@ -0,0 +1,41 @@
+#pragma once
+#define _OS_UNIX
+
+#include <BaseClientRpc.h>
+#include <Common.pb.h>
+#include <DeviceConfigClientRpc.h>
+#include <DeviceManagerClientRpc.h>
+#include <RouterClient.h>
+#include <SessionManager.h>
+#include <TransportClientTcp.h>
+#include <drake/common/drake_copyable.h>
+#include <drake/common/name_value.h>
+#include <drake/common/scoped_singleton.h>
+#include <drake/systems/analysis/simulator.h>
+#include <drake/systems/framework/leaf_system.h>
+
+namespace piplup
+{
+    namespace kinova_gen3
+    {
+        using namespace drake;
+
+        namespace k_api = Kinova::Api;
+        class Gen3HardwareInterface : public systems::LeafSystem<double>
+        {
+        public:
+            Gen3HardwareInterface();
+
+        private:
+            systems::EventStatus Initialize(const systems::Context<double> &,
+                                            systems::State<double> *);
+            void CalcUpdate(const systems::Context<double> &,
+                            systems::State<double> *) const;
+            k_api::Base::BaseClient * base_;
+            k_api::SessionManager * session_manager;
+            k_api::RouterClient * router;
+            k_api::TransportClientTcp * transport;
+        };
+
+    } // namespace kinova_gen3
+} // namespace piplup
